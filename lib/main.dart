8066b97942c921982ff7f214c1fec4a418985f55
Frank Mendez <frankmendezwebdev@gmail.com>
2020-02-27 10:56:44 +0800

    Using Private Properties

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,16 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
-    setState(() => {questionIndex = questionIndex + 1});
-    print(questionIndex);
+  void _answerQuestion() {
+    setState(() => {_questionIndex = _questionIndex + 1});
+    print(_questionIndex);
   }
 
   @override
@@ -33,11 +33,11 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(children: [
           Text(
-            questions[questionIndex],
+            questions[_questionIndex],
           ),
           RaisedButton(
             child: Text('Answer 1'),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
           ),
           RaisedButton(
             child: Text('Answer 2'),
